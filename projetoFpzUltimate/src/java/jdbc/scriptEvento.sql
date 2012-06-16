34b8ec31b7853d01dd32b6d4f9ae5296ab33fcd6
zanellidrummer <zanellidrummer@0e5a089c-f725-16e0-ab45-0045728fb495>
2012-06-16 14:35:58 +0000

    Scripts para população parcial do banco de dados.

---
+++ projetoFpzUltimate/src/java/jdbc/scriptEvento.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM projetofpz.evento
+LIMIT 0, 1000
+
+-- Date: 2012-06-16 11:35
+*/
+INSERT INTO `evento` (`id_evento`,`nome`,`dt_inicio`,`dt_fim`) VALUES (1,'Congresso Nacional do IF','30/06/2012','05/07/2012');
