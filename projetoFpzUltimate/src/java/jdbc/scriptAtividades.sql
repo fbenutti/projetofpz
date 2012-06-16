2f95f86573866382d03215b51312fe437523b2c9
zanellidrummer <zanellidrummer@0e5a089c-f725-16e0-ab45-0045728fb495>
2012-06-16 14:26:14 +0000

    Scripts para população parcial do banco de dados.

---
+++ projetoFpzUltimate/src/java/jdbc/scriptAtividades.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM projetofpz.atividade
+LIMIT 0, 1000
+
+-- Date: 2012-06-16 11:15
+*/
+INSERT INTO `atividade` (`id_atividade`,`descricao`,`vagas`,`horario_inicio`,`horario_fim`,`es_evento`,`es_responsavel`) VALUES (1,'Apresentação das atividade',30,'18:00','19:00',1,1);
+INSERT INTO `atividade` (`id_atividade`,`descricao`,`vagas`,`horario_inicio`,`horario_fim`,`es_evento`,`es_responsavel`) VALUES (2,'Palestra: jQuery em ação',30,'19:00','20:00',1,2);
+INSERT INTO `atividade` (`id_atividade`,`descricao`,`vagas`,`horario_inicio`,`horario_fim`,`es_evento`,`es_responsavel`) VALUES (3,'Mesa Redonda: Contrução de software, um novo desafio',30,'20:30','22:30',1,3);
---
+++ projetoFpzUltimate/src/java/jdbc/scriptPessoas.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM projetofpz.pessoa
+LIMIT 0, 1000
+
+-- Date: 2012-06-16 11:11
+*/
+INSERT INTO `pessoa` (`cpf`,`nome`,`email`,`cep`,`bairro`,`cidade`,`uf`,`complemento`,`numero`,`rua`,`telefone`,`es_user_name`) VALUES ('123.456.789-09','Paulin Baitola','paulinnude@gmail.com',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'123.456.789-09');
+INSERT INTO `pessoa` (`cpf`,`nome`,`email`,`cep`,`bairro`,`cidade`,`uf`,`complemento`,`numero`,`rua`,`telefone`,`es_user_name`) VALUES ('363.528.068-21','Wellington','zanellidrummer@gmail.com',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'363.528.068-21');
